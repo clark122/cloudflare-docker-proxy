73dc179e0fc31dba36c6aa620288149fe200198c
wujing <wujing@xingconnected.com>
2024-07-03 17:30:03 +0800

    update to gvs source

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.gvssmart.xyz": dockerHub,
+  "quay.gvssmart.xyz": "https://quay.io",
+  "gcr.gvssmart.xyz": "https://gcr.io",
+  "k8s-gcr.gvssmart.xyz": "https://k8s.gcr.io",
+  "k8s.gvssmart.xyz": "https://registry.k8s.io",
+  "ghcr.gvssmart.xyz": "https://ghcr.io",
+  "cloudsmith.gvssmart.xyz": "https://docker.cloudsmith.io",
+  "ecr.gvssmart.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.gvssmart.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
